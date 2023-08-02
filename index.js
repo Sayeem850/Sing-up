daedce176ec0127d2c419653ae09ffac06eccf9f
Sayeem850 <134822419+Sayeem850@users.noreply.github.com>
2023-08-02 15:11:55 +0600

    Sing up

---
+++ index.js
@@ -0,0 +1,13 @@
+let massage = document.querySelector("#massage");
+
+function mouseEvent(e) { console.log(e.key) }
+
+massage.addEventListener("keypress", mouseEvent);
+
+
+
+let pass = document.querySelector("#pass");
+
+function event(e) { console.log(e.key) }
+
+pass.addEventListener("keypress", mouseEvent)
